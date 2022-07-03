b1ecb720fcd9858fbf80b796774ba553e3ca4ac1
felipematheus1337 <lipehbr@gmail.com>
2022-07-03 16:42:56 -0300

    project created and cleaned

---
+++ backend/src/main/java/com/movieRate/MovieRateApplication.java
@@ -0,0 +1,13 @@
+package com.movieRate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieRateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieRateApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/movieRate/MovieRateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.movieRate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieRateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
