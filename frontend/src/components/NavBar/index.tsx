ae646c08cf8db625e4340b715cef31f9ea7e30ec
felipematheus1337 <lipehbr@gmail.com>
2022-07-03 19:07:37 -0300

    MovieCard

--- frontend/src/components/NavBar/index.tsx
+++ frontend/src/components/NavBar/index.tsx
@@ -4,10 +4,10 @@ function Navbar() {
     return (
         <header>
       <nav className="container">
-        <div className='movierate-nav-content'>
-          <h1 style={{margin:0}}>MovieRate</h1>
+        <div className='dsmovie-nav-content'>
+          <h1 style={{margin:0, color: 'white'}}>MovieRate</h1>
           <a href="https://github.com/felipematheus1337">
-          <div className="movierate-contact-container">
+          <div className="dsmovie-contact-container">
             <GithubIcon/>
             <p style={{margin:'0 0 0 10px'}}>/felipematheus1337</p>
           </div>
