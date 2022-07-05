54f45988eb9237d561ac40b66b21a11d144d530f
felipematheus1337 <lipehbr@gmail.com>
2022-07-04 21:26:57 -0300

    findAll and findById Added

---
+++ backend/src/main/java/com/movieRate/repositories/MovieRepository.java
@@ -0,0 +1,10 @@
+package com.movieRate.repositories;
+
+import com.movieRate.entities.Movie;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface MovieRepository extends JpaRepository<Movie,Long> {
+
+
+}
